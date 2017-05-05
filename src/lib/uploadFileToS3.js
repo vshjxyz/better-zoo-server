18247ac5c348458c010bb7374dc3a268edfe1f48
Luca Del Bianco <vshjxyz@gmail.com>
2017-05-05 14:57:15 +0200

    Changing bucket policy and content type of uploaded files

--- src/lib/uploadFileToS3.js
+++ src/lib/uploadFileToS3.js
@@ -1,11 +1,13 @@
 import AWS from 'aws-sdk'
 import fs from 'fs'
+import constants from './constants/shared'
 
 export default (filePath, bucketName, key) => {
   const s3 = new AWS.S3()
   const params = {
     Bucket: bucketName,
     Key: key,
+    ContentType: `audio/${constants.EXTENSION}`,
     Body: fs.createReadStream(filePath)
   }
 
--- terraform/buckets.tf
+++ terraform/buckets.tf
@@ -6,19 +6,29 @@ resource "aws_s3_bucket" "better-zoo" {
     "Version": "2012-10-17",
     "Statement": [
         {
-            "Sid": "Allow read and write access to bucket for specific user roles",
+            "Sid": "Allows delete and put object to the user single_user",
             "Effect": "Allow",
             "Principal": {
                 "AWS": "${aws_iam_user.single_user.arn}"
             },
             "Action": [
-                "s3:GetObject",
                 "s3:DeleteObject",
                 "s3:PutObject"
             ],
             "Resource": [
                 "arn:aws:s3:::better-zoo",
                 "arn:aws:s3:::better-zoo/*"
+            ]
+        },
+        {
+            "Sid": "Allows read access to mp3s to everyone",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::better-zoo/*.mp3"
             ]
         }
     ]

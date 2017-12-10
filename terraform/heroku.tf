13e21e5afab2f56fdf89b920ddaf11dcd508895d
Luca Del Bianco <vshjxyz@gmail.com>
2017-12-10 23:33:38 +0100

    Add Dockerfile

--- terraform/heroku.tf
+++ terraform/heroku.tf
@@ -5,10 +5,12 @@ resource "heroku_app" "default" {
   config_vars {
     AWS_ACCESS_KEY_ID = "${aws_iam_access_key.user_access_key.id}"
     AWS_SECRET_ACCESS_KEY = "${aws_iam_access_key.user_access_key.secret}"
+    BASE_URL = "http://www.105.net/upload/uploadedContent/repliche/zoo/"
   }
 
   buildpacks = [
     "heroku/nodejs",
-    "https://github.com/sunny4381/heroku-buildpack-linuxbrew"
+    "https://github.com/heroku/heroku-buildpack-apt",
+    "https://github.com/vshjxyz/heroku-buildpack-linuxbrew"
   ]
 }

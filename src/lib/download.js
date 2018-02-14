67beaf02e18fedfa2dd156fbf091fcc193f06402
Federico <federico.obialero@gmail.com>
2018-02-14 18:05:48 +0100

    added error handling for invalid url

--- src/lib/download.js
+++ src/lib/download.js
@@ -25,45 +25,43 @@ export default function download (filename, attempts = constants.MAX_ATTEMPTS) {
       stream: process.stdout
     }, progress.Presets.rect)
 
-    if (attempts === 0) {
-      reject(new Error(clk.red(`Unable to download ${fullUrl}`)))
-      return
-    }
+    const errorHandler = (e) => reject(new Error(e))
 
-    http.get(
-      fullUrl,
-      (response) => {
-        let cur = 0
-        const responseType = response.headers['content-type']
-        const total = parseInt(response.headers['content-length'], 10)
-        const totalInMB = (total / (1024 * 1024)).toFixed(2)
+    const request = http.get(
+        fullUrl,
+        (response) => {
+          let cur = 0
+          const responseType = response.headers['content-type']
+          const total = parseInt(response.headers['content-length'], 10)
+          const totalInMB = (total / (1024 * 1024)).toFixed(2)
 
-        if (response.statusCode !== 200 && !total && !responseType.includes('audio')) {
-          reject(new Error(clk.red(`No audio file found: ${clk.magenta(fullUrl)} \ntrying again in ${retryInterval.asMinutes()} minutes.`)))
-          return setTimeout(() => (
-            download(filename, attempts - 1)
-            .catch(err => console.log(err))
-          ), retryInterval.asMilliseconds())
+          if (response.statusCode !== 200 && !total && !responseType.includes('audio')) {
+            reject(new Error(`No audio file found: ${clk.magenta(fullUrl)} \ntrying again in ${retryInterval.asMinutes()} minutes.`))
+            return setTimeout(() => (
+              download(filename, attempts - 1)
+              .catch(err => console.log(err))
+            ), retryInterval.asMilliseconds())
+          }
+
+          console.log(clk.gray(`Downloading ${clk.magenta(fullUrl)}`))
+
+          UI.start(totalInMB, 0)
+
+          response.on('data', (chunk) => {
+            cur += chunk.length
+            const curInMB = (cur / (1024 * 1024)).toFixed(2)
+            UI.update(curInMB)
+          })
+
+          response.on('error', errorHandler)
+          response.on('end', () => {
+            UI.stop()
+            resolve(fullpath)
+          })
+
+          response.pipe(file)
         }
-
-        console.log(clk.gray(`Downloading ${clk.magenta(fullUrl)}`))
-
-        UI.start(totalInMB, 0)
-
-        response.on('data', (chunk) => {
-          cur += chunk.length
-          const curInMB = (cur / (1024 * 1024)).toFixed(2)
-          UI.update(curInMB)
-        })
-
-        response.on('error', reject)
-        response.on('end', () => {
-          UI.stop()
-          resolve(fullpath)
-        })
-
-        response.pipe(file)
-      }
-    )
+      )
+    request.on('error', errorHandler)
   })
 }
